047cb2c53333236990f20e3a845f4fa238e9df28
Nurul <nurulfdl672@gmail.com>
2025-07-08 14:28:14 +0700

    update

--- src/stores/profil.js
+++ src/stores/profil.js
@@ -7,19 +7,11 @@ export const useProfilStore = defineStore('profil', {
   }),
   actions: {
     async fetchProfil() {
-<<<<<<< HEAD
       const res = await axios.get('http://localhost:3000/profil')
       this.profil = res.data
     },
     async updateProfil(data) {
       const res = await axios.put('http://localhost:3000/profil', data)
-=======
-      const res = await axios.get('https://d1f440cb-fc40-4a70-ba0e-8a994b535b78-00-1sza5lvku4kaa.pike.replit.dev//profil')
-      this.profil = res.data
-    },
-    async updateProfil(data) {
-      const res = await axios.put('https://d1f440cb-fc40-4a70-ba0e-8a994b535b78-00-1sza5lvku4kaa.pike.replit.dev//profil', data)
->>>>>>> b24c2add3ccb8a8f76399a1490f696b23cf3ab8f
       this.profil = res.data
     }
   }
